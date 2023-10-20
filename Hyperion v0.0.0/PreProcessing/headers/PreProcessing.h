bc59969238c5c0e9a1133a47a11cf5f83748a235
adiirani <148150663+adiirani@users.noreply.github.com>
2023-10-19 18:10:25 -0700

    Update PreProcessing.h

--- Hyperion v0.0.0/PreProcessing/headers/PreProcessing.h
+++ Hyperion v0.0.0/PreProcessing/headers/PreProcessing.h
@@ -7,28 +7,29 @@
 #include <set>
 #include <vector>
 #include <map>
+#include <unordered_map>
 #include <algorithm>
 #include <functional>
 #include <cmath> // Include the cmath header for log function
 
 class encoders {
 public:
-    std::vector<int> label(const std::vector<std::string>& input);
-    std::vector<std::vector<int>> oneHot(const std::vector<std::string>& input);
-    std::vector<int> ordinal(const std::vector<std::string>& input, const std::vector<std::string>& order);
+    std::vector<double> label(const std::vector<std::string>& input);
+    std::vector<std::vector<double>> oneHot(const std::vector<std::string>& input);
+    std::vector<double> ordinal(const std::vector<std::string>& input, const std::vector<std::string>& order);
 };
 
 class vectorizers {
 private:
-    std::unordered_map<int, std::vector<std::string>> reverseDictionary;
+    std::unordered_map<double, std::vector<std::string>> reverseDictionary;
 
     std::vector<std::string> tokenize(const std::string& input, const std::set<std::string>& stopWords);
     size_t hash(const std::string& input);
 
 public:
-    std::vector<std::vector<int>> count(const std::vector<std::string>& input, const std::set<std::string>& stopWords);
+    std::vector<std::vector<double>> count(const std::vector<std::string>& input, const std::set<std::string>& stopWords);
     std::vector<std::vector<double>> tfidf(const std::vector<std::string>& input, const std::set<std::string>& stopWords);
-    std::vector<std::vector<int>> hashing(const std::vector<std::string>& input, const std::set<std::string>& stopWords);
+    std::vector<std::vector<double>> hashing(const std::vector<std::string>& input, const std::set<std::string>& stopWords);
     std::unordered_map<int, std::vector<std::string>> getReverseDictionary() const;
 };
 
